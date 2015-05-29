6aed4da083cdec2d8f3a0c20bbaae036ad51a764
duskdweller <none@none>
2015-05-29 14:27:43 +0200

    Closing branch in preparation for merge.
    --HG--
    branch : duskdweller-RequiredDirectoriesControl-RootPathCheck
    extra : close : 1

--- FOMod/Properties/AssemblyInfo.cs
+++ FOMod/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5624.26505")]
+[assembly: AssemblyFileVersion("1.1.5627.22369")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
